bbc808198373744289a3f86a52db41f467fb49e1
artificial1129 <414406462@qq.com>
2024-12-03 10:56:43 +0800

    完成教程学习

--- startz/first/urls.py
+++ startz/first/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 app_name = "first"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/",views.choose, name="choose"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/",views.ChooseView.as_view(), name="choose"),
     path("<int:question_id>/vote/",views.vote, name="vote"),
+    path("<int:pk>/results/",views.ResultsView.as_view(), name="results"),
 ]
--- startz/first/views.py
+++ startz/first/views.py
@@ -1,19 +1,44 @@
+from django.db.models import F
+from django.urls import reverse
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question,Choice
+from django.views import generic
 # Create your views here.
 
+class IndexView(generic.ListView):
+    template_name = "first/index.html"
+    context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")
-    context = {"latest_question_list" : latest_question_list} #上下文
-    return render(request,"first/index.html",context)
+class ChooseView(generic.DetailView):
+    model = Question
+    template_name = "first/detail.html"
 
-def choose(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    context = {"question": question_id}
-    return render(request,"first/detail.html",context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "first/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("vote")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "first/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("first:results", args=(question.id,)))
